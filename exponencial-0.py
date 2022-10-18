46d3263991471e88c2909e395fe6b84fb77a4bcf
Pojucan <22040996+pojucan@users.noreply.github.com>
2022-10-17 23:29:20 -0300

    Terceiro Commit

--- exponencial-0.py
+++ exponencial-0.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 """
-Lendo um arquivo .txt com pandas no Google Sheets e criando um pandas data 
+Lendo um arquivo .xlsx com pandas do Google Sheets e criando e abordando por data 
 frame:
 """
 # Database do Google Sheet:
@@ -24,34 +24,34 @@ url = f'https://docs.google.com/spreadsheets/d/1tcWCH0K3ZeDecAI9JFrEOvimTEgMh5Na
 # Lendo os dados brutos:
 df_brute = pd.read_csv(url)
 
-# Mostrando na tela os valores:
+# Mostrando na tela os valores de dados brutos:
 print(df_brute)
 
-# Tamanho da figura:
+# Configura o tamanho da figura:
 plt.figure(figsize=(8, 6))
 
-# Range de plotagem:
+# Plotagem do gráfico via data frame:
 plt.plot(df_brute['x'], df_brute['y'])
 
-# plotagem do rótulo do eixo alterando tamanho da fonte e estilo:
+# Escrita do rótulo do eixo X alterando tamanho da fonte e estilo:
 plt.xlabel('Abscissas (u.c.)', fontsize = 12, fontweight = 'bold')
 
-# Limita o intervalo da escala x (Dados de 1.0 à 12.0):
+# Limita o intervalo da escala X (Dados de 1.0 à 12.0):
 plt.xlim(0, 13)
 
-# Passo de 1.0 na escala x dentro do range de 1.0 à 12.0:
+# Passo de 1.0 na escala X dentro do range de 1.0 à 12.0:
 plt.yticks(range(0, 12, 1))
 
-# plotagem do rótulo do eixo alterando tamanho da fonte e estilo:
+# Escrita do rótulo do eixo Y alterando tamanho da fonte e estilo:
 plt.ylabel('Ordenadas (u.c.)', fontsize = 12, fontweight = 'bold')
 
-# Limita o intervalo da escala y (range da escala):
+# Limita o intervalo da escala Y (range da escala):
 plt.ylim(0, 4096) 
 
-# Passo de 1000.0 na escala y dentro do range de 2.0 à 4096.0:
+# Passo de 1000.0 na escala Y dentro do range de 2.0 à 4096.0:
 plt.yticks(range(0, 4300, 300))
 
-# Título do gráfico e propriedades:
+# Título do gráfico e tamanho da fonte:
 plt.title('Gráfico X .vs. Y Exponencial', fontsize = 20)
 
 # Exportação do gráfico para imagem:
