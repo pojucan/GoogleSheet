cd452f451d3437af8b69791e6073f6272dedf182
Pojucan <22040996+pojucan@users.noreply.github.com>
2022-10-17 23:33:57 -0300

    Quinto Commit

--- exponencial-0.py
+++ exponencial-0.py
@@ -10,18 +10,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 """
-Lendo um arquivo .xlsx com pandas do Google Sheets e abordando por data 
-frame:
+Lendo um arquivo .xlsx do Google Sheets com pandas e abordando por data frame:
 """
-# Database do Google Sheet:
+# ID do arquivo no Google Sheets:
 pasta_id = '1tcWCH0K3ZeDecAI9JFrEOvimTEgMh5Na'
 
-# Planilha 020 da pasta 020:
+# Planilha data da pasta data no Google Sheets:
 planilha_name = 'data'
 
 url = f'https://docs.google.com/spreadsheets/d/1tcWCH0K3ZeDecAI9JFrEOvimTEgMh5Na/gviz/tq?tqx=out:csv&sheet={planilha_name}'
 
-# Lendo os dados brutos:
+# Lendo os dados brutos no Google Sheets:
 df_brute = pd.read_csv(url)
 
 # Mostrando na tela os valores de dados brutos:
@@ -33,22 +32,22 @@ plt.figure(figsize=(8, 6))
 # Plotagem do gráfico via data frame:
 plt.plot(df_brute['x'], df_brute['y'])
 
-# Escrita do rótulo do eixo X alterando tamanho da fonte e estilo:
+# Escrita do rótulo no eixo X alterando tamanho da fonte e estilo:
 plt.xlabel('Abscissas (u.c.)', fontsize = 12, fontweight = 'bold')
 
-# Limita o intervalo da escala X (Dados de 1.0 à 12.0):
+# Limita o intervalo da escala no eixo X (Dados de 1.0 à 12.0):
 plt.xlim(0, 13)
 
-# Passo de 1.0 na escala X dentro do range de 1.0 à 12.0:
+# Passo de 1.0 na escala do eixo X dentro do range de 1.0 à 12.0:
 plt.yticks(range(0, 12, 1))
 
-# Escrita do rótulo do eixo Y alterando tamanho da fonte e estilo:
+# Escrita do rótulo no eixo Y alterando tamanho da fonte e estilo:
 plt.ylabel('Ordenadas (u.c.)', fontsize = 12, fontweight = 'bold')
 
-# Limita o intervalo da escala Y (range da escala):
+# Limita o intervalo da escala no eixo Y (range da escala):
 plt.ylim(0, 4096) 
 
-# Passo de 1000.0 na escala Y dentro do range de 2.0 à 4096.0:
+# Passo de 1000.0 na escala do eixo Y dentro do range de 2.0 à 4096.0:
 plt.yticks(range(0, 4300, 300))
 
 # Título do gráfico e tamanho da fonte:
